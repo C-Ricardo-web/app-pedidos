d07bfe682d9ef254824974c527520c478fc30c33
C-Ricardo-web <cristobalyandres@gmail.com>
2026-08-04 03:17:52 -0500

    commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname)));
 // Conexión a Supabase
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY,
+  process.env.SUPABASE_SECRET_KEY
 );
 
 // Rutas para tus páginas
